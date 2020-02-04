2419290d1df960080311ed28b151ad01a7abb8d0
Andrew Katsikas <apkatsikas@gmail.com>
2020-02-04 00:23:03 -0500

    Update kill.bat

--- utils/kill.bat
+++ utils/kill.bat
@@ -1,1 +1,3 @@
+REM This script executes a PowerShell command to list every process that has an open window in windows
+REM And kill those processes
 powershell -command "(New-Object -comObject Shell.Application).Windows() | foreach-object {$_.quit()}; Get-Process | Where-Object {$_.MainWindowTitle -ne \"\"} | stop-process"
